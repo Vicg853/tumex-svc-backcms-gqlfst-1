68916baea0b5a11446ed56e0db5f0643a9ee4910
Vicg853 <vicg853@gmail.com>
2022-05-28 23:31:03 -0300

    Adding projects ts type

---
+++ src/resolvers/projects/types.ts
@@ -0,0 +1,33 @@
+import {
+   Project,
+   Techs
+} from '@prisma-gen/type-graphql'
+
+import {
+   Locales
+} from '../types'
+
+export interface ProjectsFullResultType {
+   id: Project['id'];
+   createdAt: Project['createdAt'];
+   
+   title: Project['title'];
+   description: Project['description']
+   
+   scopes: Project['scopes']
+   topics?: Project['topics']
+   image?: Project['image']
+   
+   resources?: Project['resources']
+   ghRepo?: Project['ghRepo']
+   website?: Project['website']
+   relatedProjects?: ProjectsFullResultType[]
+   techStack?: (Techs|any)[] //TODO don't forget about adding this type TechStack[]
+   
+   startDate: Project['startDate']
+   endDate?: Project['endDate']
+
+   hidden: Project['hidden']
+   archived: Project['archived']
+}
+
