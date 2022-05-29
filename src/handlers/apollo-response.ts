f12c25109568d1cfb88c13234388d8f56905a093
Vicg853 <vicg853@gmail.com>
2022-05-29 09:48:51 -0300

    Adding new error message for invalid prisma relation reference and adding http cats to errors

--- src/handlers/apollo-response.ts
+++ src/handlers/apollo-response.ts
@@ -1,8 +1,13 @@
 import type { Config } from 'apollo-server-core'
 
+const httpCatsUrl = process.env.HTTP_CATS_URL
+
 export const formatError: Config['formatError'] = (error) => {
    const defaultErr = {
       message: error.message,
+      extensions: {
+         httpCatCode: `${httpCatsUrl}/500`
+      }
    }
 
    //* Auth related errors
@@ -11,15 +16,27 @@ export const formatError: Config['formatError'] = (error) => {
       path: error.path,
       extensions: {
          code: '401',
+         httpCatCode: `${httpCatsUrl}/401`
       }
    }
 
+   
    //* Other errors
    if(!error.extensions || !error.extensions.code) return defaultErr
    else if(error.extensions.code === 'INTERNAL_SERVER_ERROR') {
       console.error(JSON.stringify(error, null, 2))
       return {
          message: 'Internal server error! This is our fault sorry!',
+         extensions: {
+            httpCatCode: `${httpCatsUrl}/500`
+         }
+      }
+   } 
+   else if(error.extensions.code === 'P2025') return {
+   message: 'Invalid relation input: One or more of the provided Database relations do not exist or are invalid.',
+      extensions: {
+         code: '424',
+         httpCatCode: `${httpCatsUrl}/424`
       }
    }
 
