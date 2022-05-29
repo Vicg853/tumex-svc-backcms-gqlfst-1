4c40b1048fbaa459bc19c5a5a62f46444fc02950
Vicg853 <vicg853@gmail.com>
2022-05-29 20:01:02 -0300

    Adding deep relation to queries

--- src/resolvers/projects/queries.ts
+++ src/resolvers/projects/queries.ts
@@ -1,131 +1,116 @@
 import type { ApolloContext } from '~/index'
-import type { Prisma } from '@prisma/client'
-import type { ProjectsFullResultType } from './types'
 
-import { Authorized, ObjectType, Field, Resolver, Query, Ctx } from 'type-graphql'
-import {
-   Resource,
-   ProjectScopes,
-   Techs
-} from '@prisma-gen/type-graphql'
+import Fields from 'graphql-fields'
+import { Info, Resolver, Query, Ctx, Args } from 'type-graphql'
 
-@ObjectType("Project", {
-   isAbstract: true
-})
-export class ProjectFullResult {
-   //TODO Revise auth scopes
-   @Authorized("SUDO")
-   @Field(_type => String, {
-      nullable: false,
-      description: "The project's id"
-   })
-   id!: ProjectsFullResultType['id']
-
-   //TODO Revise auth scopes
-   @Authorized("SUDO")
-   @Field(_type => String, {
-      nullable: false,
-      description: "The project's createdAt date"
-   })
-   createdAt!: ProjectsFullResultType['createdAt']
-
-   @Field(_type => String, {
-      nullable: false,
-      description: "The project's title"
-   })
-   title!: ProjectsFullResultType['title']
-
-   @Field(_type => String, {
-      nullable: false,
-      description: "The project's description"
-   })
-   description!: ProjectsFullResultType['description']
-
-   @Field(_type => ProjectScopes, {
-      nullable: false,
-      description: 'Project\'s scope'
-   })
-   scopes!: ProjectsFullResultType['scopes'];
-
-   @Field(_type => [String], {
-      nullable: true,
-      description: 'Project\'s topics (e.g.: Backend, Frontend, etc.)'
-   })
-   topics?: ProjectsFullResultType['topics'];
-
-   @Field(_type => String, {
-      nullable: true,
-      description: "The project's image"
-   })
-   image!: ProjectsFullResultType['image']
-
-   @Field(_type => [Resource], {
-      nullable: true,
-      description: 'Project\'s related resources'
-   })
-   resources?: Resource[];
-
-   @Field(_type => String, {
-      nullable: true,
-      description: "The project's github repository"
-   })
-   ghRepo!: ProjectsFullResultType['ghRepo']
-
-   @Field(_type => [String], {
-      nullable: true,
-      description: 'Project\'s websites'
-   })
-   website?: ProjectsFullResultType['website'];
-
-   //TODO Figure this out
-   //@Field(_type => [this], {
-   //   nullable: true,
-   //   description: 'Project\'s other related projects'
-   //})
-   //relatedProjectIds?: this[];
-
-   //TODO Don't forget about adding this type TechStack[]
-   @Field(_type => [Techs], {
-      nullable: true
-   })
-   relatedTechStackIds?: Techs[]; //TODO don't forget about adding this type TechStack[]
-
-   @Field(_type => Date, {
-      nullable: false,
-      description: 'Project\'s start date'
-   })
-   startDate!: ProjectsFullResultType['startDate'];
-
-   @Field(_type => Date, {
-      nullable: true,
-      description: 'Project\'s end date. Leave undefined in case project is still ongoing.'
-   })
-   endDate?: ProjectsFullResultType['endDate'];
-
-   //TODO Revise auth scopes
-   @Authorized("SUDO")
-   @Field(_type => Boolean, {
-      nullable: false
-   })
-   hidden?: boolean;
-  
-   //TODO Revise auth scopes
-   @Authorized("SUDO")
-   @Field(_type => Boolean, {
-      nullable: false
-   })
-   archived?: boolean;
-}
+import { ProjectResultAndRels } from './classes/projectCreate'
+import { OpacityRelatedProjectArgs, ProjectQueryArgs } from './classes/queryArgs'
 
 @Resolver()
 export class ProjectsQueriesResolver {
-   @Query(_type => [ProjectFullResult], {
-      nullable: false,
+   @Query(_type => [ProjectResultAndRels], {
+      nullable: true,
       description: 'Get all projects'
    })
    async projects(
       @Ctx() ctx: ApolloContext,
-   ): Promise<ProjectFullResult[]> {
-      return await ctx.prisma.project.findMany()
+      @Args() { 
+         includeArchived, onlyArchived,
+         includeHidden, onlyHidden
+      }: OpacityRelatedProjectArgs,
+      @Info() info: any
+   ): Promise<ProjectResultAndRels[]> {
+       //TODO Improve this when its inheritance resolvers is ready
+      //* To prevent useless queries and data transfers from/to the database,
+      //* we need to retrieve the query requested fields, map those to 
+      //* the db relational include queries. 
+      //* In case they are not defined we simply return undefined, this way prisma
+      //* won't even bother thinking about them.
+      //* Quite a janky way to do it, but it works until proper 
+      //* field resolvers for these are implemented
+      const fields = Fields(info)
+      const showRelatedProjs = fields['relatedProject'] ? { relatedProjects:{ 
+            select: { id: false, relatedTo: true, relatedId: false, project: false, projectId: false }
+      } } : undefined
+      const showRelatedTo = fields['relatedTo'] ? {  relatedTo: {
+            select: { id: false, project: true, projectId: false, relatedTo: false, relatedId: false }
+      } } : undefined
+      const showTechStack = fields['techStack'] ? { techStack: { 
+         select: { id: false, tech: true, techId: false, project: false, projectId: false }
+      } } : undefined
+
+      const prisma = await ctx.prisma.project.findMany({
+         where: {
+            archived: onlyArchived || (includeArchived ? undefined : false),
+            hidden: onlyHidden || (includeHidden ? undefined : false)
+         },
+         include: {
+            ...showRelatedProjs as unknown as { relatedProjects: { select: { relatedTo: true } } },
+            ...showRelatedTo as unknown as { relatedTo: { select: { project: true } } },
+            ...showTechStack as unknown as { techStack: { select: { tech: true } }},
+         }
+      })
+
+      return prisma.map(project => ({
+         ...project,
+         relatedProject: showRelatedProjs ? 
+            project.relatedProjects.map(childProj => ({ ...childProj['relatedTo'] })) : undefined,
+         techStack: showTechStack ? 
+            project.techStack.map(tech => ({ ...tech['tech'] })) : undefined,
+         relatedTo: showRelatedTo ? 
+            project.relatedTo.map(childProj => ({ ...childProj['project'] })) : undefined,
+      }))
+   }
+
+   @Query(_type => ProjectResultAndRels, {
+      nullable: true,
+      description: 'Get a project by id'
+   })
+   async project(
+      @Ctx() ctx: ApolloContext,
+      @Args() {id, includeHidden}: ProjectQueryArgs,
+      @Info() info: any
+   ): Promise<ProjectResultAndRels | null> {
+      //TODO Improve this when its inheritance resolvers is ready
+      //* To prevent useless queries and data transfers from/to the database,
+      //* we need to retrieve the query requested fields, map those to 
+      //* the db relational include queries. 
+      //* In case they are not defined we simply return undefined, this way prisma
+      //* won't even bother thinking about them.
+      //* Quite a janky way to do it, but it works until proper 
+      //* field resolvers for these are implemented
+      const fields = Fields(info)
+      const showRelatedProjs = fields['relatedProject'] ? { relatedProjects:{ 
+            select: { id: false, relatedTo: true, relatedId: false, project: false, projectId: false }
+      } } : undefined
+      const showRelatedTo = fields['relatedTo'] ? {  relatedTo: {
+            select: { id: false, project: true, projectId: false, relatedTo: false, relatedId: false }
+      } } : undefined
+      const showTechStack = fields['techStack'] ? { techStack: { 
+         select: { id: false, tech: true, techId: false, project: false, projectId: false }
+      } } : undefined
+
+      const res = await ctx.prisma.project.findUnique({
+         where: {
+            id, 
+         },
+         rejectOnNotFound: false,
+         include: {
+            ...showRelatedProjs as unknown as { relatedProjects: { select: { relatedTo: true } } },
+            ...showRelatedTo as unknown as { relatedTo: { select: { project: true } } },
+            ...showTechStack as unknown as { techStack: { select: { tech: true } }},
+         }
+      })
+
+      if (!res || (res.hidden && !includeHidden)) {
+         return null
+      }
+
+      return {
+         ...res,
+         relatedProject: showRelatedProjs ? res.relatedProjects.map(proj => ({ ...proj['relatedTo'] })) : undefined,
+         techStack: showTechStack ? res.techStack.map(tech => ({ ...tech['tech'] })) : undefined,
+         relatedTo: showRelatedTo ? res.relatedTo.map(proj => ({ ...proj['project'] })) : undefined,
+      }
    }
 }
