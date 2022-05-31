7c3550d60622b5c565a855021150300a7b601fd8
Vicg853 <vicg853@gmail.com>
2022-05-31 18:36:31 -0300

    Filter args split and goballization

---
+++ src/resolvers/projects/classes/filterArgs.ts
@@ -0,0 +1,37 @@
+import { ArgsType, Authorized, Field, InputType } from 'type-graphql'
+import {
+   ProjectResult
+} from './queryFields'
+
+@InputType('ProjectGlobalFilters', {
+   isAbstract: true
+})
+export class ProjGlobalFilterArgs {
+   @Field(_type => Boolean, {
+      nullable: true,
+      description: "If true, returns archived and non-archived projects."
+   })
+   includeArchived?: boolean
+
+   @Field(_type => Boolean, {
+      nullable: true,
+      description: "If true, returns only archived projects. Overrides \"includeArchived\"."
+   })
+   onlyArchived?: boolean
+
+   //TODO Revise auth scopes
+   @Authorized('SUDO')
+   @Field(_type => Boolean, {
+      nullable: true,
+      description: "If true, returns only hidden projects. Overrides \"includeHidden\". Requires special permissions."
+   })
+   onlyHidden?: boolean
+
+   //TODO Revise auth scopes
+   @Authorized('SUDO')
+   @Field(_type => Boolean, {
+      nullable: true,
+      description: "If true, returns archived and non-archived projects. Requires special permissions."
+   })
+   includeHidden?: boolean
+}
