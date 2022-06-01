795b83956b9cad5c3307e3dd4a2c1dd78e527256
Vicg853 <vicg853@gmail.com>
2022-05-31 23:00:36 -0300

    Added create rel mutation

---
+++ src/resolvers/projects/classes/relCreateArgs.ts
@@ -0,0 +1,22 @@
+import { ArgsType, Field, InputType } from 'type-graphql'
+
+@ArgsType()
+export class ProjectRelationCreate {
+   @Field(_type => String, {
+      nullable: false,
+      description: 'Project id that will be linked to other projects'
+   })
+   id!: string
+
+   @Field(_type => [String], {
+      nullable: true,
+      description: 'Project(s) id(s) to which the project will be linked as relatee'
+   })
+   asRelateeTo?: string[]
+
+   @Field(_type => [String], {
+      nullable: true,
+      description: 'Project(s) id(s) that will be linked as relatee(s) to the project'
+   })
+   relatees?: string[]
+}
