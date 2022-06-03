a16ede5cab7e0da04f09a9bc554964d8e93b693b
Vicg853 <vicg853@gmail.com>
2022-06-03 17:52:48 -0300

    Added delete mutation

---
+++ src/resolvers/objectives/delete.ts
@@ -0,0 +1,48 @@
+import type { ApolloContext } from '~/index'
+
+import { ApolloError } from 'apollo-server-core'
+import {
+   Arg,
+   Resolver,
+   Mutation,
+   Authorized,
+   Ctx
+} from 'type-graphql'
+
+import {
+   CreateObjectiveArgs
+} from './classes/createArgs'
+
+@Resolver()
+export class ObjectiveRemoveResolvers {
+   @Authorized('SUDO')
+   @Mutation(_type => String, {
+      nullable: false,
+      description: 'Remove one or many objectives'
+   })
+   async removeObjectives(
+      @Ctx() ctx: ApolloContext,
+      @Arg('ids') ids: string[]
+   ): Promise<string> {
+      const prismaRes = await ctx.prisma.objectives.deleteMany({
+         where: {
+            id: {
+               in: ids
+            }
+         }
+      }).then(res => ({
+         count: res.count,
+         message: null,
+         err: null
+      })).catch(err => ({
+         count: 0,
+         message: err.message ?? 'An unknown error occurred while removing objectives',
+         err: err.code ?? 'INTERNAL_SERVER_ERROR'
+      }))
+
+      if(prismaRes.err)
+         throw new ApolloError(prismaRes.message, prismaRes.err)
+
+      return `${prismaRes.count} objectives removed`
+   }
+}
