094fdd4b1540c3a9f04abd67adb334aedeef76d6
Vicg853 <vicg853@gmail.com>
2022-06-08 23:59:46 -0300

    Added query only resolver

--- src/resolvers/techs/query.ts
+++ src/resolvers/techs/query.ts
@@ -4,14 +4,13 @@ import type { ApolloContext } from '~/index'
 import { ApolloError } from 'apollo-server-core'
 import {
    Args,
-   Authorized,
    Ctx,
    Query,
    Resolver
 } from 'type-graphql'
 
 import { QueryFields } from './classes/queryFields'
-import { QueryArgs } from './classes/queryArgs'
+import { QueryManyArgs, QueryOnlyArgs } from './classes/queryArgs'
 
 @Resolver()
 export class TechQueryResolver {
@@ -20,7 +19,7 @@ export class TechQueryResolver {
    })
    async getManyTechs(
       @Ctx() ctx: ApolloContext,
-      @Args() { filter, /*group*/ includeHidden, onlyHidden }: QueryArgs
+      @Args() { filter, /*group*/ includeHidden, onlyHidden }: QueryManyArgs
    ): Promise<Techs[] | null> {
       const prismaRes = await ctx.prisma.techs.findMany({
          where: {
@@ -41,5 +40,42 @@ export class TechQueryResolver {
          throw new ApolloError(prismaRes.message, prismaRes.err)
 
       return prismaRes.data
+   }
+
+   @Query(_type => QueryFields, {
+      description: 'Query a specific tech by its ID or name'
+   })
+   async getTech(
+      @Ctx() ctx: ApolloContext,
+      @Args() { id, name, includeHidden, onlyHidden }: QueryOnlyArgs
+   ): Promise<Techs | null> {
+      if(!id && !name)
+         throw new ApolloError('You must provide either an ID or a name to query a tech', '406')
+
+      const prismaRes = await ctx.prisma.techs.findUnique({
+         where: {
+            ...(id ? { id } : {}),
+            ...(name ? { name } : {}),
+         },
+         rejectOnNotFound: false
+      }).then(res => ({
+         data: res,
+         message: null,
+         err: null
+      })).catch(err => ({
+         data: null,
+         message: err.message ?? 'An unknown error occurred while creating new techs',
+         err: err.code ?? 'INTERNAL_SERVER_ERROR'
+      }))
+
+      if(prismaRes.err)
+         throw new ApolloError(prismaRes.message, prismaRes.err)
+
+      if(!prismaRes.data)
+         return null
+
+      return onlyHidden ? prismaRes.data : 
+         ((!prismaRes.data.hidden || includeHidden) ? 
+         prismaRes.data : null)
    }
 }
