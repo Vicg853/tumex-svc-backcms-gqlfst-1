3f77b5b0c63592606c63cad78efa67741aac12eb
Vicg853 <vicg853@gmail.com>
2022-06-18 16:52:58 -0300

    Added updated many transaction mutation to objectives update

--- src/resolvers/objectives/mod.ts
+++ src/resolvers/objectives/mod.ts
@@ -13,7 +13,8 @@ import { Scopes } from '@config/jwt-tkn'
 import { AuthMiddle } from '@middlewares/auth'
 import { 
    ObjectiveBulkModArgs,
-   ObjectiveModInput 
+   ObjectiveModInput,
+   ManyObjectivesModArgs
 } from './classes/modArgs'
 import {
    ObjectiveQueryFields
@@ -50,13 +51,13 @@ export class ObjectiveModResolver {
       }))
 
       if(prismaRes.err !== null)
-         throw new Error(prismaRes.message, prismaRes.err)
+         throw new ApolloError(prismaRes.message, prismaRes.err)
 
       return prismaRes.data
    }
 
    @AuthMiddle({
-      scopes: [Scopes.objectivesEdit, Scopes.objectivesHiddenEdit]
+      scopes: [Scopes.objectivesEdit]
    })
    @Mutation(() => String, {
       description: 'Update an objective',
@@ -87,9 +88,48 @@ export class ObjectiveModResolver {
       }))
 
       if(prismaRes.err !== null) 
-         throw new Error(prismaRes.message, prismaRes.err)
+         throw new ApolloError(prismaRes.message, prismaRes.err)
 
       return prismaRes.count! > 0 ? `Updated ${prismaRes.count} objective${prismaRes.count! > 1 ? 's' : ''}.` 
          : 'No objectives were updated.'
+   }
+
+   @AuthMiddle({
+      scopes: [Scopes.objectivesEdit]
+   })
+   @Mutation(() => [ObjectiveQueryFields], {
+      description: 'Update many objectives at once but separately.',
+      nullable: false,
+   })
+   async modManyObjectives(
+      @Ctx() ctx: ApolloContext,
+      @Args() { shared, data: rawData }: ManyObjectivesModArgs
+   ): Promise<ObjectiveQueryFields[] | null> {
+      const prismaTransaction = await ctx.prisma.$transaction(rawData.map(({ id, ...rawObjectiveData }) => 
+         ctx.prisma.objectives.update({
+            where: { id },
+            data: {
+               ...rawObjectiveData,
+               year: rawObjectiveData.year ?? shared?.year,
+               progress: rawObjectiveData.progress ?? shared?.progress,
+               hidden: rawObjectiveData.hidden ?? shared?.hidden,
+               ...(rawObjectiveData.title && {title: { update: rawObjectiveData.title }}),
+               ...(rawObjectiveData.description && {description: { update: rawObjectiveData.description }}),
+            }
+         })
+      )).then(data => ({
+         data,
+         err: null,
+         message: null
+      })).catch(error => ({
+         data: null,
+         err: error.code,
+         message: error.message
+      }))
+
+      if(prismaTransaction.err !== null)
+         throw new ApolloError(prismaTransaction.message, prismaTransaction.err)
+
+      return prismaTransaction.data
    }
 }
